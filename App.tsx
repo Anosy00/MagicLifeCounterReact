6196fc6f54b7fc7209eb6f1213ef6232a30a3363
Lucas <lucas.theodore2004@gmail.com>
2024-01-24 10:44:03 +0100

    FirstCommit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+var leftLife: number = 20;
+let rightLife: number = 20;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={[styles.container]}>
+      <View style={[styles.leftSide, styles.lifeArea]}>
+        <Pressable style={styles.button}></Pressable>
+        <Pressable style={styles.button}></Pressable>
+        <Text style={styles.lifeTotalText}>{leftLife}</Text>
+        
+      </View>
+      <View style={[styles.rightSide, styles.lifeArea]}>
+        <Text style={styles.lifeTotalText}>{rightLife}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    width: "100%"
   },
+
+  leftSide: {
+    backgroundColor: 'green',
+    width: '50%',
+    height: '100%'
+  },
+  rightSide: {
+    backgroundColor: 'red',
+    width: '50%',
+    height: '100%'
+  },
+  lifeArea: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  lifeTotalText:{
+    fontSize: 60,
+    position: 'absolute',
+    maxWidth: 100
+  },
+  button:{
+    backgroundColor: 'black',
+    width: "100%",
+    height: "100%"
+  }
 });
+
---
+++ Button.tsx
@@ -0,0 +1,8 @@
+import { Pressable, Text, StyleSheet, styles } from "react-native";
+export default function Button() {
+    return (
+      <Pressable style={styles.button}>
+        <Text></Text>
+      </Pressable>
+    );
+  }
